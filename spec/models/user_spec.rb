bef4a68becbdcb041db1e76d8e0b28b52af2f4ef
Brian Jerardi <bjerardi@gmail.com>
2012-05-18 10:33:28 -0400

    Add tests for password and confimation hashes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,8 @@ require 'spec_helper'
 describe User do
   
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
   end
   
   subject { @user }
@@ -22,6 +23,8 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   
   it { should be_valid }
   
@@ -39,6 +42,7 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+  
   
   describe "when email format is invalid" do
     it "should be invalid" do
@@ -68,6 +72,22 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+  
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 end
