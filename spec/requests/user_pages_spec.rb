52885eae97f1cfe7b15365b5a44cfdbbbfe93b19
Brian Jerardi <bjerardi@gmail.com>
2012-05-18 22:27:55 -0400

    Add tests for error messages.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,18 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Password is too short ' + 
+              '(minimum is 6 characters)') }
+        it { should have_content('Password confirmation can\'t be blank') }
+      end
     end
     
     describe "with valid information" do
