38d83c547646789e4148d66617748b4a72153155
Brian Jerardi <bjerardi@gmail.com>
2012-05-23 11:12:50 -0400

    Add sign_in method.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,7 +21,7 @@ describe "Authentication" do
     
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user) }
+      before { sign_in user }
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
@@ -32,6 +32,25 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+      end
+    end
+  end
+  
+  describe "authorization" do
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      
+      describe "in the Users controller" do
+        
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+        
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
       end
     end
   end
