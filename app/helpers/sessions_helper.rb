4d11f2f8afacddfee560e533ae3f9838fbf63ee7
Brian Jerardi <bjerardi@gmail.com>
2012-05-23 11:27:18 -0400

    Add tests and ability to require the right user to edit.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,12 @@ module SessionsHelper
     @current_user = user
   end
   
-  def current_user
+   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def current_user?(user)
+    user == current_user
   end
   
   def sign_out
